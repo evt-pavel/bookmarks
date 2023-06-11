94e539513f2952368423567667a42090be0a5bd2
pwl-ev <pwl93.e@gmail.com>
2023-06-11 23:05:30 +0300

    add customers view, follow def

--- account/urls.py
+++ account/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
      path('register/', views.register, name='register'),
      path('edit/', views.edit, name='edit'),
      path('social-auth/', include('social_django.urls', namespace='social')),
+     path('users/', views.user_list, name='user_list'),
+     path('users/follow/', views.user_follow, name='user_follow'),
+     path('users/<username>/', views.user_detail, name='user_detail'),
+     
 ]
 
 if settings.DEBUG:
--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.urls import reverse_lazy
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail',
+                                        args=[u.username])
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
